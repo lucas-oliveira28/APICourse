a698d65242d31abad87c09a0a64c0fa00ca28ddf
Lucas Monteiro <lucas.monteiro280898@gmail.com>
2025-07-22 21:55:28 -0300

    Project created

---
+++ src/test/java/com/lucasmonteiro/apicourse/ApiCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmonteiro.apicourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
