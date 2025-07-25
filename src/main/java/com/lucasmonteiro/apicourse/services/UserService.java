1a633a2112fb0b890871d28cb49d249f78cbb4fd
Lucas Monteiro <lucas.monteiro280898@gmail.com>
2025-07-25 20:13:48 -0300

    Exception handling - findById

--- src/main/java/com/lucasmonteiro/apicourse/services/UserService.java
+++ src/main/java/com/lucasmonteiro/apicourse/services/UserService.java
@@ -2,10 +2,12 @@ package com.lucasmonteiro.apicourse.services;
 
 import com.lucasmonteiro.apicourse.entities.User;
 import com.lucasmonteiro.apicourse.repositories.UserRepository;
+import com.lucasmonteiro.apicourse.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,7 +20,8 @@ public class UserService {
     }
 
     public User findById(Long id) {
-        return repository.findById(id).get();
+        Optional<User> user = repository.findById(id);
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
---
+++ src/main/java/com/lucasmonteiro/apicourse/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.lucasmonteiro.apicourse.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found: Id " + id);
+    }
+}
